13d15f717ee11473df0fc67d7f1399001f1f0936
3DIS-HARDES\Hardes <hardes@3dis.de>
2026-01-13 13:15:52 +0100

    init

---
+++ coi-serviceworker.js
@@ -0,0 +1,70 @@
+/*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
+let coepCredentialless = false;
+if (typeof window === 'undefined') {
+  self.addEventListener("install", () => self.skipWaiting());
+  self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+  self.addEventListener("message", (ev) => {
+    if (!ev.data) return;
+    if (ev.data.type === "deregister") {
+      self.registration.unregister().then(() => {
+        return self.clients.matchAll();
+      }).then(clients => {
+        clients.forEach(client => client.navigate(client.url));
+      });
+    } else if (ev.data.type === "coepCredentialless") {
+      coepCredentialless = ev.data.value;
+    }
+  });
+
+  self.addEventListener("fetch", function (event) {
+    const r = event.request;
+    if (r.cache === "only-if-cached" && r.mode !== "same-origin") return;
+
+    const request = (coepCredentialless && r.mode === "no-cors")
+      ? new Request(r, { credentials: "omit" })
+      : r;
+
+    event.respondWith(
+      fetch(request).then((response) => {
+        if (response.status === 0) return response;
+
+        const newHeaders = new Headers(response.headers);
+        newHeaders.set("Cross-Origin-Embedder-Policy", coepCredentialless ? "credentialless" : "require-corp");
+        if (!newHeaders.get("Cross-Origin-Opener-Policy")) newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: newHeaders,
+        });
+      }).catch((e) => console.error(e))
+    );
+  });
+} else {
+  (() => {
+    const reloadedBySelf = window.sessionStorage.getItem("coiReloadedBySelf");
+    window.sessionStorage.removeItem("coiReloadedBySelf");
+    const coepDegrading = (reloadedBySelf == "coepdegrade");
+
+    if (window.crossOriginIsolated || coepDegrading) return;
+
+    const n = navigator;
+    if (n.serviceWorker) {
+        n.serviceWorker.register(window.document.currentScript.src).then(
+        (registration) => {
+            console.log("COI Service Worker registered");
+            registration.addEventListener("updatefound", () => {
+                console.log("Reloading to activate COI Service Worker...");
+                window.location.reload();
+            });
+            if (registration.active && !n.serviceWorker.controller) {
+                console.log("Reloading to control page...");
+                window.location.reload();
+            }
+        },
+        (err) => { console.error("COI Service Worker registration failed: ", err); }
+        );
+    }
+  })();
+}
